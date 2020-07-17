c1567c87efe59769f753bd34539d126c6c96bf1e
Maryam <Sedaghati85@gmail.com>
2020-07-17 14:36:18 +0430

    add map

--- app/src/main/java/gardeshgari/gardeshgari/osm/OsmActivity.java
+++ app/src/main/java/gardeshgari/gardeshgari/osm/OsmActivity.java
@@ -1,15 +1,113 @@
 package gardeshgari.gardeshgari.osm;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 import gardeshgari.gardeshgari.R;
+import gardeshgari.gardeshgari.utils.BaseActivity;
 
-public class OsmActivity extends AppCompatActivity {
+@EActivity(R.layout.activity_osm)
+public class OsmActivity extends BaseActivity {
+
+    @ViewById
+    MapView map;
+
+    @AfterViews
+    void init(){
+        //presenter.attachview(this);
+
+        getPermition();
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(18.5);
+
+        Configuration.getInstance().setUserAgentValue(getPackageName());
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//        double longitude = location.getLongitude();
+//        double latitude = location.getLatitude();
+
+        GeoPoint startPoint = new GeoPoint(35.717878 , 51.412612 );
+        mapController.setCenter(startPoint);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+        map.invalidate();
+        startMarker.setIcon(getResources().getDrawable(R.drawable.pin));
+        startMarker.setTitle("hhhhhhh");
+        startMarker.showInfoWindow();
+
+
+
+    }
+
+//    public void onResume() {
+//        super.onResume();
+//        //this will refresh the osmdroid configuration on resuming.
+//        //if you make changes to the configuration, use
+//        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+//        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+//    }
+
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_osm);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    public void getPermition() {
+        if (ContextCompat.checkSelfPermission(mcontext, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mactivity, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1500);
+        }
+        if (ContextCompat.checkSelfPermission(mcontext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mactivity, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1500);
+        }
+        if (ContextCompat.checkSelfPermission(mcontext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(mactivity, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1500);
+        }
+    }
+
 }
